d003288af9ab4c1fb2e1ed31e63d45a87d0834d4
= <2316683971@qq.com>
2022-03-28 17:59:13 +0800

    完成基本功能待优化

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import time
 import pygame
 from pygame.locals import *
+import copy
 import words
 
 BORDERWIDTH = 4  # 游戏区边框宽度
@@ -42,7 +43,7 @@ def main():
     cur_pos_x, cur_pos_y = 0, 0
 
     game_area = None  # 整个游戏区域
-    game_area_img = {} # 对应game_area区域的图片
+    game_area_img = {}  # 对应game_area区域的图片
     game_over = True
     start = False  # 是否开始，当start = True，game_over = True 时，才显示 GAME OVER
     score = 0  # 得分
@@ -51,34 +52,101 @@ def main():
     pause = False  # 暂停
     last_drop_time = None  # 上次下落时间
     last_press_time = None  # 上次按键时间
-    cur_block = words.get_block()
+    cur_block = None
 
     def _dock():
         nonlocal cur_block, next_block, game_area, game_area_img, cur_pos_x, cur_pos_y, game_over, score, speed
-        cur_block = words.get_block()
+        if cur_pos_y <= 0 and cur_pos_y not in (-1, -2):
+            game_over = True
+
+        cur_block = next_block
+        next_block = words.get_block()
         cur_pos_x, cur_pos_y = (BOARDWIDTH - cur_block.end_pos.X - 1) // 2, -1 - cur_block.end_pos.Y
+
+        # 判断消除
+        temp_dic = {}
+        for key, value in game_area_img.items():
+            temp_dic[key] = value
+
+        for key, value in temp_dic.items():
+            # 林
+            if value.next == 1 and value.name == '2' and key[0] > 0 and key[0] < BOARDHEIGHT:
+                for key_,value_ in temp_dic.items():
+                    if key_ == key:
+                        continue
+                    if (key_[0] == key[0] + 1 or key_[0] == key[0] - 1) and value_.next == 1 and value_.name == '2':
+                        if key_ in game_area_img.keys():
+                            del game_area_img[key_]
+                            score += 10
+                        if key in game_area_img.keys():
+                            del game_area_img[key]
+                            score += 10
+                    if (key_[0] == key[0] + 1):
+                        for i in range(key[1], key[1] + 2):
+                            for j in range(key[0], key[0] + 2):
+                                game_area[i][j] = '.'
+                    if (key_[0] == key[0] - 1):
+                        for i in range(key[1], key[1] + 2):
+                            for j in range(key[0] - 1, key[0] + 1):
+                                game_area[i][j] = '.'
+
+        for key, value in temp_dic.items():
+            # 昌
+            if value.next == 1 and value.name == '3':  # 小的在上
+                for key_, value_ in temp_dic.items():
+                    if key_ == key:
+                        continue
+                    if (key_[1] == key[1] + 2 and (key[0] == key_[0] or key[0] == key_[0] + 1)) and value_.next == 2 and value_.name == '3':
+                        if key_ in game_area_img.keys():
+                            del game_area_img[key_]
+                            score += 10
+                        if key in game_area_img.keys():
+                            del game_area_img[key]
+                            score += 10
+
+                    if key_[1] == key[1] + 2 and key[0] == key_[0]:
+                        for i in range(key[1], key[1] + 2):
+                            for j in range(key[0], key[0] + 1):
+                                game_area[i][j] = '.'
+                        for i in range(key_[1], key_[1] + 1):
+                            for j in range(key[0], key[0] + 2):
+                                game_area[i][j] = '.'
+
+                    if key_[1] == key[1] + 2 and key[0] - 1 == key_[0]:
+                        for i in range(key[1], key[1] + 2):
+                            for j in range(key[0], key[0] + 1):
+                                game_area[i][j] = '.'
+                        for i in range(key_[1], key_[1] + 1):
+                            for j in range(key[0] - 1, key[0] + 1):
+                                game_area[i][j] = '.'
+
 
 
 
     def _judge(pos_x, pos_y, block):
-        if pos_x < 0 or pos_x >= BOARDWIDTH or pos_x + block.end_pos.X - block.start_pos.X >= BOARDWIDTH: #判断左右横移越界情况
+        if pos_x < 0 or pos_x >= BOARDWIDTH or pos_x + block.end_pos.X - block.start_pos.X >= BOARDWIDTH or (pos_x > 0 and game_area[pos_y][pos_x] == 'O'):
             return False
+        if pos_x + block.end_pos.X - block.start_pos.X < BOARDWIDTH and game_area[pos_y][pos_x + block.end_pos.X - block.start_pos.X] == 'O':
+            return False
+
         if pos_y == BOARDHEIGHT - 1:
-            game_area_img[pos_x , pos_y - (block.end_pos.Y - block.start_pos.Y)] = block  #存当前block左上角坐标以及block，用于后续绘制
-            for i in range(pos_y - (block.end_pos.Y - block.start_pos.Y),pos_y + 1):
-                for j in range(pos_x,pos_x + block.end_pos.X - block.start_pos.X + 1):
+            game_area_img[pos_x, pos_y - (block.end_pos.Y - block.start_pos.Y)] = block
+            for i in range(pos_y - (block.end_pos.Y - block.start_pos.Y), pos_y + 1):
+                for j in range(pos_x, pos_x + block.end_pos.X - block.start_pos.X + 1):
                     game_area[i][j] = 'O'
             _dock()
         else:
-            for i in range(pos_x,pos_x + block.end_pos.X - block.start_pos.X + 1):
+            for i in range(pos_x, pos_x + block.end_pos.X - block.start_pos.X + 1):
                 if game_area[pos_y + 1][i] == 'O':
                     # print(game_area[pos_y + 1][pos_x:(block.end_pos.X - block.start_pos.X + 1)])
                     # sys.exit()
-                    game_area_img[pos_x, pos_y - (block.end_pos.Y - block.start_pos.Y)] = block  # 存当前block左上角坐标以及block，用于后续绘制
-                    for i in range(pos_y - (block.end_pos.Y - block.start_pos.Y),pos_y + 1):
-                        for j in range(pos_x,pos_x + block.end_pos.X - block.start_pos.X + 1):
+                    game_area_img[
+                        pos_x, pos_y - (block.end_pos.Y - block.start_pos.Y)] = block  # 存当前block左上角坐标以及block，用于后续绘制
+                    for i in range(pos_y - (block.end_pos.Y - block.start_pos.Y), pos_y + 1):
+                        for j in range(pos_x, pos_x + block.end_pos.X - block.start_pos.X + 1):
                             game_area[i][j] = 'O'
                     _dock()
+
         return True
 
     while True:
@@ -98,25 +166,38 @@ def main():
                         cur_block = words.get_block()
                         next_block = words.get_block()
                         cur_pos_x, cur_pos_y = (BOARDWIDTH - cur_block.end_pos.X - 1) // 2, -1 - cur_block.end_pos.Y
-                if event.key == pygame.K_UP:
+
+                elif event.key == K_SPACE:
+                    if not game_over:
+                        pause = not pause
+
+                elif event.key == pygame.K_UP:
                     if 0 <= cur_pos_x <= BOARDWIDTH - (cur_block.end_pos.Y - cur_block.start_pos.Y) - 1:
                         _next_block = words.get_next_block(cur_block)
                         cur_block = _next_block
-                if event.key == pygame.K_LEFT:
-                    if not game_over and not pause and _judge(cur_pos_x - 1, cur_pos_y, cur_block):
-                        if time.time() - last_press_time > 0.1:
-                            last_press_time = time.time()
-                            cur_pos_x -= 1
-                if event.key == pygame.K_RIGHT:
-                    if not game_over and not pause and _judge(cur_pos_x + 1, cur_pos_y, cur_block):
-                        if time.time() - last_press_time > 0.1:
-                            last_press_time = time.time()
-                            cur_pos_x += 1
-                if event.key == pygame.K_DOWN:
-                    if not game_over and not pause and _judge(cur_pos_x, cur_pos_y + 1, cur_block):
-                        if time.time() - last_press_time > 0.1:
-                            last_press_time = time.time()
-                            cur_pos_y += 1
+
+        if event.type == pygame.KEYDOWN:  # 长按方向减移动有效
+            if event.key == pygame.K_LEFT:
+                if not game_over and not pause and _judge(cur_pos_x - 1, cur_pos_y, cur_block):
+                    if time.time() - last_press_time > MOVESIZEWAYSFREQ:
+                        last_press_time = time.time()
+                        cur_pos_x -= 1
+            if event.key == pygame.K_RIGHT:
+                if not game_over and not pause and _judge(cur_pos_x + 1, cur_pos_y, cur_block):
+                    if time.time() - last_press_time > MOVESIZEWAYSFREQ:
+                        last_press_time = time.time()
+                        cur_pos_x += 1
+            if event.key == pygame.K_DOWN:
+                if not game_over and not pause and _judge(cur_pos_x, cur_pos_y + 1, cur_block):
+                    if time.time() - last_press_time > MOVEDOWNFREQ:
+                        last_press_time = time.time()
+                        cur_pos_y += 1
+
+        _draw_background(screen)
+        _draw_gridlines(screen)
+        _draw_block(screen, cur_pos_x * BOXSIZE, cur_pos_y * BOXSIZE, cur_block)
+        _draw_info(screen, font1, font_pos_x, font1_height, score)
+        _draw_game_area(screen, game_area_img)
 
         if not game_over:  # 控制block自动下落速度
             cur_drop_time = time.time()
@@ -134,12 +215,8 @@ def main():
                            (WINDOWWIDTH - gameover_size[0]) // 2, (WINDOWHEIGHT - gameover_size[1]) // 2,
                            'GAME OVER', RED)
 
-        _draw_background(screen)
-        _draw_gridlines(screen)
-        _draw_block(screen, cur_pos_x * BOXSIZE, cur_pos_y * BOXSIZE, cur_block)
-        _draw_info(screen, font1, font_pos_x, font1_height, score)
-        _draw_game_area(screen,game_area_img)
-        print(game_area)
+        _draw_block(screen, font_pos_x, 30 + (font1_height + 6) * 5, next_block, )
+
         pygame.display.flip()
 
 
@@ -155,15 +232,17 @@ def _draw_background(screen):
 
 # 画单个方块
 def _draw_block(screen, pos_x, pos_y, block):
-    image = pygame.image.load(block.image)
-    screen.blit(pygame.transform.scale(image, (
-    (block.end_pos.X - block.start_pos.X + 1) * BOXSIZE, (block.end_pos.Y - block.start_pos.Y + 1) * BOXSIZE)),
-                (pos_x, pos_y))
+    if not block is None:
+        image = pygame.image.load(block.image)
+        screen.blit(pygame.transform.scale(image, (
+            (block.end_pos.X - block.start_pos.X + 1) * BOXSIZE, (block.end_pos.Y - block.start_pos.Y + 1) * BOXSIZE)),
+                    (pos_x, pos_y))
+
 
 # 画已经落下的方块
-def _draw_game_area(screen,game_area_img):
-    for key,value in game_area_img.items():
-        _draw_block(screen,key[0] * BOXSIZE,key[1]  * BOXSIZE,value)
+def _draw_game_area(screen, game_area_img):
+    for key, value in game_area_img.items():
+        _draw_block(screen, key[0] * BOXSIZE, key[1] * BOXSIZE, value)
 
 
 # 画网格线
--- words.py
+++ words.py
@@ -7,14 +7,25 @@ Block = namedtuple('Block', 'image start_pos end_pos name next')
 # 土字相关偏旁——编号为1,
 # 木字相关偏旁——编号为2,
 # 日字相关偏旁——编号为3
-TU_BLOCK = [Block("images/tu1.png", Point(0, 0), Point(0, 1), "1", 1),
-            Block("images/tu2.png", Point(0, 0), Point(1, 0), "1", 0),]
+TU_BLOCK = [Block("images/tu.png", Point(0, 0), Point(0, 1), "1", 1),
+            Block("images/tu.png", Point(0, 0), Point(1, 0), "1", 2),
+            Block("images/tu.png", Point(0, 0), Point(1, 1), "1", 0)
+            ]
 
-MU_BLOCK = [Block("images/mu1.png", Point(0, 0), Point(0, 1), "2", 1),
-            Block("images/mu2.png", Point(0, 0), Point(1, 0), "2", 0),]
+MU_BLOCK = [Block("images/mu.png", Point(0, 0), Point(0, 1), "2", 1),
+            Block("images/mu.png", Point(0, 0), Point(1, 0), "2", 2),
+            Block("images/mu.png", Point(0, 0), Point(1, 1), "2", 0)]
 
-RI_BLOCK = [Block("images/ri1.png", Point(0, 0), Point(0, 1), "3", 1),
-            Block("images/ri2.png", Point(0, 0), Point(1, 0), "3", 0),]
+RI_BLOCK = [Block("images/ri.png", Point(0, 0), Point(0, 1), "3", 1),
+            Block("images/ri.png", Point(0, 0), Point(1, 0), "3", 2),
+            Block("images/ri.png", Point(0, 0), Point(1, 1), "3", 0)]
+
+# 触发消除的汉字
+# 晶
+# 林
+# 杳
+# 昌
+# 杲
 
 BLOCKS = {'1': TU_BLOCK,
           '2': MU_BLOCK,
@@ -23,7 +34,7 @@ BLOCKS = {'1': TU_BLOCK,
 
 
 def get_block():
-    block_name = random.choice('123')
+    block_name = random.choice('23')
     b = BLOCKS[block_name]
 
     idx = random.randint(0, len(b) - 1)
